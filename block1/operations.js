8e29330e0947fb26b3d428995eb24ecbfefac3a9
Ricardo Campos <ricard_halo@live.com.mx>
2022-06-02 18:07:07 -0500

    Continue with basic operations

---
+++ block1/operations.js
@@ -0,0 +1,50 @@
+// assignment
+var a = 1;
+var b = 2;
+
+// sum
+var sum = a + b;
+// subtraction
+var sub = a - b;
+// multiplication
+var mult = a * b;
+// division
+var divi = a / b;
+// module
+var module = a % b;
+// exponentiation
+var exp = a ** b;
+
+a += 1;
+// other method increase or decrease by 1
+a ++;
+a --;
+
+console.log(a);
+
+// text
+var name = 'Ricardo';
+var last_name = 'Campos';
+
+var complete_name = name + ' ' + last_name;
+console.log(complete_name);
+// other method
+name += ' ';
+name += last_name;
+console.log(name);
+
+// combine operations with different types
+var number = 2;
+var text = 'Hello';
+var sum_combine = number + text;
+
+console.log(sum_combine);
+
+// Exercise class
+// Program tell us "good morning"
+// Output: 'Buenos dias NAME'
+
+var text_morning = "Good morning";
+var name_user = "Ricardo";
+
+console.log(text_morning + " " + name_user +"!!");
---
+++ block1/variables.js
@@ -0,0 +1,19 @@
+var box_a = 2;
+var box_b = 5;
+var box_c = "Hello World";
+var box_d = true;
+
+console.log(box_a, box_c, box_d);
+
+// Exercise class
+// Swap value variables, x:2, y:3
+// Output: x: 3, y: 2
+
+var x = 2;
+var y = 3;
+// Method Richard
+var temp_x;
+temp_x = x;
+console.log('Resultado ejercicio: [x] es = ',x = y,', [y] es = ',y = temp_x);
+// Method deestructuring
+[x, y] = [y, x]
