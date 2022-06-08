e063e4fd02c8b6dd309a7a434222b477a42f0821
Ricardo Campos <ricard_halo@live.com.mx>
2022-06-08 15:09:11 -0500

    all data type and examples

---
+++ block1/data_type.js
@@ -0,0 +1,26 @@
+
+// Type Javascript:
+
+var number = 2.5
+var string = 'Soy un texto';
+var boolean = true;
+var array = ["aa", "bb", 3, [1, 2]];
+var object = {
+    name: 'Martin',
+    lastname: 'Cristobal'
+}
+
+console.log(object.lastname);
+console.log(array[3]);
+
+var define_function = function(){}
+
+// Null and undefined
+// Null don't have define value
+var value_null = null;
+var valueundefined;
+console.log(typeof value_null, typeof valueundefined);
+
+// dynamic type
+// typeof
+console.log(typeof boolean);
