4ee189de5fcd20ee77916d2df49cb0a3eb5e559f
Ricardo Campos <ricard_halo@live.com.mx>
2022-06-26 13:52:49 -0500

    add examples using readline

--- block3/index.js
+++ block3/index.js
@@ -6,13 +6,20 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question('Que edad tienes?', function(answer) {
-    // TODO: Log the answer in a database
-    if (answer < 18) {
-        console.log('Eres menor de edad, largo de aqui');
-    } else {
-        console.log('Pase pase, gracias por venir');
-    }
+rl.question('Que edad tienes?', function(answer_age) {
+    rl.question('Cual es tu nombre?', function(answer_name){
+        if (answer_age >= 18){
+            console.log('Hola ' + answer_name + ', eres mayor de edad');
+        } else {
+            console.log('Hola ' + answer_name + ', eres menor de edad');
+        }
+        rl.close();
+    });
+})
 
+rl.question('Cuantas iteraciones quieres hacer?', function(iterations){
+    for (let i = 1; i <= iterations; i++){
+        console.log(i);
+    }
     rl.close();
 })
