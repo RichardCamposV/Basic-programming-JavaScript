91648aeeedbaa70c27249f37312a53459e09460c
Ricardo Campos <ricard_halo@live.com.mx>
2022-06-28 19:05:37 -0500

    Add second mode - mark done tasks

--- project_mastermind/index.js
+++ project_mastermind/index.js
@@ -1,5 +1,6 @@
 
 const readline = require('readline');
+const internal = require('stream');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -11,38 +12,82 @@ function add_task(task_list, task_description){
 
 function print_task_list(task_list){
 
+    number_index = 1;
+
     console.log('Tareas:');
     for(let task of task_list){
 
         if (task.done){
-            console.log('[x]' + ' ' + task.description);
+            console.log('#' + number_index + ' [x]' + ' ' + task.description);
         } else {
-            console.log('[ ]' + ' ' + task.description);
+            console.log('#' + number_index + ' [ ]' + ' ' + task.description);
         }
+        number_index += 1;
     }
 }
 
+function mark_task_as_done(task_list, index){
+
+    if (index >= 0 && index < Object.keys(task_list).length) {
+        task_list[index].done = true;
+    } else {
+        console.log('Invalid task number');
+    }
+}
+
+function check_all_done(task_list){
+    for(let task of task_list){
+        if (!task.done) return false;
+    }
+    return true;
+}
+
 // first mode: read necesary tasks
-function ask_for_tasks(task_list) {
-    rl.question('\nIntroduce una nueva tarea ( [S] - para salir )\n', function(task_desc){
+function mode1(task_list) {
+    rl.question('\nIntroduce una nueva tarea ( [s] - para salir )\n-', function(task_desc){
 
         switch(task_desc) {
-            case 'S':
+            case 's':
                 console.log('\nNo se introducen ya mas tareas');
-                rl.close();
+                mode2(task_list);
                 break;
             default:
                 add_task(task_list, task_desc);
                 console.log('\nLa lista de tareas actual es:');
                 print_task_list(task_list);
-                ask_for_tasks(task_list);
+                mode1(task_list);
                 break;
         }
         
     });
 }
 
+// second mode: mark done tasks
+
+function mode2(task_list) {
+    print_task_list(task_list);
+    rl.question('\nQue tarea has realizado? (1 - N) - [ Escribe (exit) para salir ]\n*', function(task_number){
+        switch(task_number) {
+            case 'exit':
+                console.log('bye bye...');
+                rl.close;
+                break;
+            default:
+                mark_task_as_done(task_list, task_number - 1);
+                // Check if all tasks are complete and close the program
+                check_tasks = check_all_done(task_list);
+                if (check_tasks === false){
+                    mode2(task_list);
+                } else {
+                    print_task_list(task_list);
+                    console.log('\nTodas las tareas se han hecho, NICE!!!');
+                    rl.close;
+                }
+                break;
+        }
+    });
+}
+
 let task_list = [];
 
-ask_for_tasks(task_list);
-// second mode: mark done tasks
+mode1(task_list);
